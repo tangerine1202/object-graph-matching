8a32fdf87e6091cbf7b39e727788671b58d30817
AlanHuang <alan0988469026@gmail.com>
2023-10-25 21:12:41 +0800

    add visualization fn

---
+++ viz_utils.py
@@ -0,0 +1,104 @@
+import pickle as pkl
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2
+
+from eva import compute_corr
+
+
+def read_img(step, data_dir, type='sem_seg'):
+    return cv2.cvtColor(cv2.imread(f'{data_dir}/data/{type}/step{step}.png'), cv2.COLOR_BGR2RGB)
+
+
+def read_graph(step, data_dir):
+    return pkl.load(open(f'{data_dir}/graph/step{step}.pkl', 'rb'))
+
+
+def viz_corr(
+    pred_dict, data_dict,
+    data_dir,
+    img_type='inst_seg',
+    TP_CORR_COLOR=(0, 255, 0),
+    FP_CORR_COLOR=(255, 200, 0),
+    FN_CORR_COLOR=(0, 200, 255),
+    NON_CORR_COLOR=(200, 200, 200),
+):
+
+    def concat_imgs(imgs: list, axis=1, pad_px: int = 0, pad_color: list = (255, 255, 255)):
+        if pad_px > 0:
+            pad_shape = (pad_px, imgs[0].shape[1], imgs[0].shape[2]) if axis == 0 else (
+                imgs[0].shape[0], pad_px, imgs[0].shape[2])
+            padding = np.full(pad_shape, pad_color, dtype=np.uint8)
+            return np.concatenate([np.concatenate([x, padding], axis=axis) for x in imgs[:-1]] + [imgs[-1]], axis=axis)
+        else:
+            return np.concatenate(imgs, axis=axis)
+
+    def draw_bbox(img, bbox_xywh, color=(255, 0, 0), thickness=2):
+        bbox_xywh = bbox_xywh.astype(np.int32)
+        return cv2.rectangle(img, (bbox_xywh[0], bbox_xywh[1]), (bbox_xywh[0] + bbox_xywh[2], bbox_xywh[1] + bbox_xywh[3]), color, thickness)
+
+    tp_corr, fp_corr, fn_corr, gt_corr = compute_corr(pred_dict, data_dict)
+    g1_step = data_dict['g1_step'].item()
+    g2_step = data_dict['g2_step'].item()
+    g1_img = read_img(g1_step, data_dir, img_type)
+    g2_img = read_img(g2_step, data_dir, img_type)
+    pad_px = 10
+    img = concat_imgs([g1_img, g2_img], pad_px=pad_px)
+
+    g1_g = read_graph(g1_step, data_dir)
+    g2_g = read_graph(g2_step, data_dir)
+    g1_bbox = pd.DataFrame({k: g1_g.node_feat[k].squeeze(1) for k in ['bbox_cx', 'bbox_cy', 'bbox_w', 'bbox_h']})
+    g2_bbox = pd.DataFrame({k: g2_g.node_feat[k].squeeze(1) for k in ['bbox_cx', 'bbox_cy', 'bbox_w', 'bbox_h']})
+    g1_bbox['bbox_x'] = g1_bbox['bbox_cx'] - g1_bbox['bbox_w'] / 2
+    g1_bbox['bbox_y'] = g1_bbox['bbox_cy'] - g1_bbox['bbox_h'] / 2
+    g2_bbox['bbox_x'] = g2_bbox['bbox_cx'] - g2_bbox['bbox_w'] / 2
+    g2_bbox['bbox_y'] = g2_bbox['bbox_cy'] - g2_bbox['bbox_h'] / 2
+
+    for i in range(len(g1_g)):
+        bbox1 = g1_bbox[['bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']].iloc[i]
+        if i not in tp_corr and i not in fp_corr:  # and i not in fn_corr:
+            img = draw_bbox(img, bbox1, color=NON_CORR_COLOR)
+    for i in range(len(g2_g)):
+        bbox2 = g2_bbox[['bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']].iloc[i]
+        bbox2['bbox_x'] += g1_img.shape[1] + pad_px
+        if i not in tp_corr.values() and i not in fp_corr.values():  # and i not in fn_corr.values():
+            img = draw_bbox(img, bbox2, color=NON_CORR_COLOR)
+
+    for i, j in fn_corr.items():
+        bbox1 = g1_bbox[['bbox_cx', 'bbox_cy', 'bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']].iloc[i].astype(int)
+        bbox2 = g2_bbox[['bbox_cx', 'bbox_cy', 'bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']].iloc[j].astype(int)
+        bbox2[['bbox_cx', 'bbox_x']] += g1_img.shape[1] + pad_px
+        img = cv2.line(img,
+                       (bbox1['bbox_cx'], bbox1['bbox_cy']),
+                       (bbox2['bbox_cx'], bbox2['bbox_cy']),
+                       FN_CORR_COLOR,
+                       2)
+        draw_bbox(img, bbox1[['bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']], color=FN_CORR_COLOR)
+        draw_bbox(img, bbox2[['bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']], color=FN_CORR_COLOR)
+
+    for i, j in tp_corr.items():
+        bbox1 = g1_bbox[['bbox_cx', 'bbox_cy', 'bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']].iloc[i].astype(int)
+        bbox2 = g2_bbox[['bbox_cx', 'bbox_cy', 'bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']].iloc[j].astype(int)
+        bbox2[['bbox_cx', 'bbox_x']] += g1_img.shape[1] + pad_px
+        img = cv2.line(img,
+                       (bbox1['bbox_cx'], bbox1['bbox_cy']),
+                       (bbox2['bbox_cx'], bbox2['bbox_cy']),
+                       TP_CORR_COLOR,
+                       2)
+        draw_bbox(img, bbox1[['bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']], color=TP_CORR_COLOR)
+        draw_bbox(img, bbox2[['bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']], color=TP_CORR_COLOR)
+
+    for i, j in fp_corr.items():
+        bbox1 = g1_bbox[['bbox_cx', 'bbox_cy', 'bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']].iloc[i].astype(int)
+        bbox2 = g2_bbox[['bbox_cx', 'bbox_cy', 'bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']].iloc[j].astype(int)
+        bbox2[['bbox_cx', 'bbox_x']] += g1_img.shape[1] + pad_px
+        img = cv2.line(img,
+                       (bbox1['bbox_cx'], bbox1['bbox_cy']),
+                       (bbox2['bbox_cx'], bbox2['bbox_cy']),
+                       FP_CORR_COLOR,
+                       2)
+        draw_bbox(img, bbox1[['bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']], color=FP_CORR_COLOR)
+        draw_bbox(img, bbox2[['bbox_x', 'bbox_y', 'bbox_w', 'bbox_h']], color=FP_CORR_COLOR)
+
+    return img, tp_corr, fp_corr, fn_corr, gt_corr
